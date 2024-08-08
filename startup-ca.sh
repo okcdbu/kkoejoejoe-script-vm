c5b2247f354bca496b5d5cbc88ef2317404367ee
okcdbu <56333668+okcdbu@users.noreply.github.com>
2024-08-08 17:56:19 +0900

    latest2

--- startup-ca.sh
+++ startup-ca.sh
@@ -45,10 +45,15 @@ if sudo docker-compose --version; then
                 echo "Docker Compose 설치 실패"
                     exit 1
 fi
+# 공용 ip /etc/host 에 fabric-ca 추가 필요
+FABRIC_CA_ADDRESS=$1
+echo "Updating /etc/hosts with fabric-ca address..."
+sudo -- sh -c "echo '$FABRIC_CA_ADDRESS fabric-ca' >> /etc/hosts"
 
 # Hyperledger Fabric 설치
 echo "Hyperledger Fabric 2.2 설치 중..."
 curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.0 1.4.7 0.4.22
+# fabric-samples/test-network/organization/ordererOrg/fabric-*.yaml csr host fabric-ca 추가
 curl -L -o $PWD/fabric-samples/test-network/docker/docker-compose-ca.yaml https://raw.githubusercontent.com/okcdbu/kkoejoejoe-script-vm/main/docker-compose-ca.yaml
 # fabric-samples/test-network/docker 디렉토리로 이동
 echo "fabric-samples/test-network/docker 디렉토리로 이동 중..."
@@ -57,6 +62,5 @@ cd ~/fabric-samples/test-network/docker || { echo "디렉토리 이동 실패"; 
 # Docker Compose를 사용하여 CA 컨테이너 실행
 echo "Docker Compose를 사용하여 CA 컨테이너 실행 중..."
 sudo docker-compose -f docker-compose-ca.yaml up -d
-# 공용 ip /etc/host 에 fabric-ca 추가 필요
-# fabric-samples/test-network/organization/ordererOrg/fabric-*.yaml csr host fabric-ca 추가 필요
+
 echo "스크립트 실행 완료!"
