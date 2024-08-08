30259307d91f52d8c18b789c41d83d31025a90e1
okcdbu <56333668+okcdbu@users.noreply.github.com>
2024-08-08 17:53:19 +0900

    latest1

--- startup-ca.sh
+++ startup-ca.sh
@@ -24,7 +24,7 @@ sudo apt-get install -y docker-ce=5:27.0.2-1~ubuntu.20.04~focal docker-ce-cli=5:
 # Docker 서비스 시작 및 부팅 시 자동 시작 설정
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo chmod 666 /var/run/docker.sock
+
 # Docker 설치 확인
 if sudo docker --version; then
             echo "Docker 설치 성공"
@@ -49,7 +49,7 @@ fi
 # Hyperledger Fabric 설치
 echo "Hyperledger Fabric 2.2 설치 중..."
 curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.0 1.4.7 0.4.22
-
+curl -L -o $PWD/fabric-samples/test-network/docker/docker-compose-ca.yaml https://raw.githubusercontent.com/okcdbu/kkoejoejoe-script-vm/main/docker-compose-ca.yaml
 # fabric-samples/test-network/docker 디렉토리로 이동
 echo "fabric-samples/test-network/docker 디렉토리로 이동 중..."
 cd ~/fabric-samples/test-network/docker || { echo "디렉토리 이동 실패"; exit 1; }
@@ -57,6 +57,6 @@ cd ~/fabric-samples/test-network/docker || { echo "디렉토리 이동 실패"; 
 # Docker Compose를 사용하여 CA 컨테이너 실행
 echo "Docker Compose를 사용하여 CA 컨테이너 실행 중..."
 sudo docker-compose -f docker-compose-ca.yaml up -d
-# 공용 ip /etc/host 에 fabric-orderer, fabric-ca 추가 필요
+# 공용 ip /etc/host 에 fabric-ca 추가 필요
 # fabric-samples/test-network/organization/ordererOrg/fabric-*.yaml csr host fabric-ca 추가 필요
 echo "스크립트 실행 완료!"
