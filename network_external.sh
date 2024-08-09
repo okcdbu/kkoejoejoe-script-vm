a7fbd7a045f58043205166a6f0b5fe74860ffeee
okcdbu <56333668+okcdbu@users.noreply.github.com>
2024-08-09 17:48:26 +0900

    first dev clear

--- network_external.sh
+++ network_external.sh
@@ -59,3 +59,4 @@ if [ $res -ne 0 ]; then
 fi
 sudo docker-compose -f docker/docker-compose-test-net.yaml up -d orderer.example.com peer0.org1.example.com peer0.org2.example.com 2>&1
 sudo docker ps -a
+./scripts/createChannel.sh mychannel 3 5 false
