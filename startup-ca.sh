7dff58b335d25ee0d09b8030446181af053dfc02
okcdbu <56333668+okcdbu@users.noreply.github.com>
2024-08-12 17:30:13 +0900

    asf

--- startup-ca.sh
+++ startup-ca.sh
@@ -37,7 +37,7 @@ fi
 echo "Docker Compose 설치 중..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
+sudo chmod 666 /var/run/docker.sock
 # Docker Compose 설치 확인
 if sudo docker-compose --version; then
             echo "Docker Compose 설치 성공"
