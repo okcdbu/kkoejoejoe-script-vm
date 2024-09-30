9f9987e6f718374e3c9d5117d6947e5d13ca5b1d
okcdbu <56333668+okcdbu@users.noreply.github.com>
2024-09-30 18:12:11 +0900

    aslk,saf

--- network_external.sh
+++ network_external.sh
@@ -64,5 +64,20 @@ sleep 60
 wget https://raw.githubusercontent.com/okcdbu/kkoejoejoe-script-vm/main/scmsinstall.sh -O scmsinstall.sh
 # 2. 다운로드한 파일에 실행 권한 부여
 chmod +x scmsinstall.sh
+# fabric-samples/test-network 디렉토리로 이동
+cd fabric-samples/test-network/
+
+# 환경 변수 설정
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+export PATH=$PATH:${PWD}/../bin/
+export FABRIC_CFG_PATH=${PWD}/../config/
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+# 상위 디렉토리로 이동
+cd ../../
 # 3. scmsinstall.sh 실행
 ./scmsinstall.sh
