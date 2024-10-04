fee0f916ac6576048cfdff845fa299dc4f303a5f
okcdbu <56333668+okcdbu@users.noreply.github.com>
2024-10-04 15:05:40 +0900

    docker elastic service

--- scmsinstall.sh
+++ scmsinstall.sh
@@ -9,21 +9,8 @@ sudo tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
 # 2. Go 환경 변수 설정
 export PATH=$PATH:/usr/local/go/bin
 echo $PATH
-# 3. ElasticSearch GPG Key 추가
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 
-# 4. ElasticSearch 패키지 저장소 추가
-sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" > /etc/apt/sources.list.d/elastic-8.x.list'
-
-# 5. 시스템 패키지 업데이트
-sudo apt-get update
-
-# 6. ElasticSearch 설치
-sudo apt-get install elasticsearch -y
-
-# 7. ElasticSearch 서비스 시작 및 자동 시작 설정
-sudo systemctl enable elasticsearch
-sudo systemctl start elasticsearch
+sudo docker run -d --name elasticsearch -p 9200:9200 -e "discovery.type=single-node" -e "xpack.security.enabled=false" docker.elastic.co/elasticsearch/elasticsearch:8.9.0
 
 # 8. GitHub에서 FabricSCMS 클론
 git clone https://github.com/okcdbu/FabricSCMS.git
